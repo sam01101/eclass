3d04718410dc43ce4f2f2720f8063e69288ebdd4
sooxiaotong <sooxiaotong@gmail.com>
2017-07-05 22:30:40 +0800

    Simple Regex

--- api/class_list.php
+++ api/class_list.php
@@ -13,14 +13,15 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_COOKIE, "PHPSESSID=".$_GET['token']);
 curl_setopt($ch,CURLOPT_USERAGENT,'CHKL');
 $result = curl_exec($ch);
-$result = preg_replace('/(\>)\s*(\<)/m', '$1$2', $result);
+//$result = preg_replace('/(\>)\s*(\<)/m', '$1$2', $result);
 //$result = preg_replace('/\s+/', ' ', $result); // make sure there aren't multiple spaces //
 //$result = str_replace("\n", '', $result);
 //var_dump($result);
 //preg_match_all('/indextabclasslist">(.*)<\/a>/U', $result, $class_list);
 //preg_match_all("/fe_eclass\('(.*)'\)/U", $result, $class_id);
 
-$regex = preg_match_all('/fe_eclass\(\'(.*)\'\)(?:.*)indextabclasslist">(.*)<\/a>(?:.*)icon_econtent.gif(?:.*)>(.*) \n<(?:.*)icon_assessment.gif(?:.*)>(.*) \n<(?:.*)icon_forum_off.gif(?:.*)>(.*) \n<(?:.*)icon_annou_off.gif(?:.*)>(.*) \n</Us', $result, $data);
+//$regex = preg_match_all('/fe_eclass\(\'(.*)\'\)(?:.*)indextabclasslist">(.*)<\/a>(?:.*)icon_econtent.gif(?:.*)>(.*) \n<(?:.*)icon_assessment.gif(?:.*)>(.*) \n<(?:.*)icon_forum_off.gif(?:.*)>(.*) \n<(?:.*)icon_annou_off.gif(?:.*)>(.*) \n</Us', $result, $data);
+$regex = preg_match_all('/fe_eclass\(\'(.*)\'\)(?:.*)indextabclasslist">(.*)<\/a>/Us', $result, $data);
 
 //var_dump($data);
 
@@ -34,10 +35,10 @@ foreach($data[1] as $key => $id)
 {
 	$class[$key]['id'] = $id;
     $class[$key]['name'] = htmlspecialchars_decode($data[2][$key]);
-    $class[$key]['c'] = $data[3][$key];
-    $class[$key]['w'] = $data[4][$key];
-    $class[$key]['b'] = $data[5][$key];
-    $class[$key]['a'] = $data[6][$key];
+    //$class[$key]['c'] = $data[3][$key];
+    //$class[$key]['w'] = $data[4][$key];
+    //$class[$key]['b'] = $data[5][$key];
+    //class[$key]['a'] = $data[6][$key];
 }
 $JSON -> count = count($class);
 $JSON -> classroom = $class;
