906e2e7f8eeff02b97371e5af0c0bce3de667f8c
sooxiaotong <sooxiaotong@gmail.com>
2017-07-06 00:04:04 +0800

    ENT_QUOTES

--- api/class_list.php
+++ api/class_list.php
@@ -34,7 +34,7 @@ $JSON = (object)array();
 foreach($data[1] as $key => $id)
 {
 	$class[$key]['id'] = $id;
-    $class[$key]['name'] = htmlspecialchars_decode($data[2][$key]);
+    $class[$key]['name'] = htmlspecialchars_decode($data[2][$key], ENT_QUOTES);
     //$class[$key]['c'] = $data[3][$key];
     //$class[$key]['w'] = $data[4][$key];
     //$class[$key]['b'] = $data[5][$key];
